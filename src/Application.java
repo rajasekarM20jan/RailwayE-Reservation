2dab0a0f8a749895fb324be1be297a81c62b53fb
Temp-user4 <rs20jan1998s@gmail.com>
2022-09-27 14:27:55 +0530

    n

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.util.Calendar;
-
+//---------Execution Logic----------
 public class Application extends Operations{
     Application() throws Exception {
     }
     public static void main(String[] args) throws Exception {
         Application o=new Application();
-        new Operations();
-        o.ticketBooking();
+        new Operations(); //calling constructor from operations class
+        o.ticketBooking(); //calling method of operations class
     }
 }
